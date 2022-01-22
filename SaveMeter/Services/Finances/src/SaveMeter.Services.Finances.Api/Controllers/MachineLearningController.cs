47b686ab9b55a69c6948a21f02efec535f290ca3
Mateusz Wróblewski <wrobmat@gmail.com>
2022-01-22 16:37:59 +0100

    setup docker-compose, create asp.net 6.0 api

---
+++ SaveMeter/Services/Finances/src/SaveMeter.Services.Finances.Api/Controllers/MachineLearningController.cs
@@ -0,0 +1,35 @@
+﻿using Instapp.Services.Finances.Application.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Instapp.Services.Finances.Application.Commands.TrainNetwork;
+
+namespace SaveMeter.Services.Finances.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class MachineLearningController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public MachineLearningController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("train")]
+        public async Task<IActionResult> Post([FromBody] TrainNetworkCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+
+    }
+}
+
---
+++ SaveMeter/Services/Finances/src/SaveMeter.Services.Finances.Api/Csv/IngCsvMapper.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration;
+using Instapp.Services.Finances.Application.Commands.CreateTransaction;
+
+namespace SaveMeter.Services.Finances.Api.Csv
+{
+    public sealed class IngCsvMapper : ClassMap<CreateTransactionCommand>
+    {
+        public IngCsvMapper()
+        {
+            //Map(x => x.AccountNumber).Name("Numer rachunku/karty");
+            Map(x => x.TransactionDate).Name("Data transakcji");
+            Map(x => x.SettlementDate).Name("Data księgowania");
+            Map(x => x.RelatedAccountNumber).Name("Nr rachunku");
+            Map(x => x.Customer).Name("Dane kontrahenta");
+            Map(x => x.Description).Name("Tytuł");
+            Map(x => x.Value).Name("Kwota transakcji (waluta rachunku)").Default(0);
+            Map(x => x.AccountBalance).Name("Saldo po transakcji");
+        }
+    }
+}
