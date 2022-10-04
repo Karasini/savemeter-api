a4aa66ed79c5663b1fed20c90b946e5aa545b066
Mateusz Wróblewski <wrobmat@gmail.com>
2022-10-04 22:16:38 +0200

    Goals module

---
+++ src/Modules/Goals/SaveMeter.Modules.Goals.Api/Controllers/GoalsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SaveMeter.Shared.Abstractions.Dispatchers;
+
+namespace SaveMeter.Modules.Goals.Api.Controllers;
+
+[Authorize(Policy)]
+internal class GoalsController : ControllerBase
+{
+    private const string Policy = "goals";
+    
+    private readonly IDispatcher _dispatcher;
+
+    public GoalsController(IDispatcher dispatcher)
+    {
+        _dispatcher = dispatcher;
+    }
+}
