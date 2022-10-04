0258d8268996992fd566fd619122262b0d4c7cda
Mateusz Wróblewski <wrobmat@gmail.com>
2022-10-04 22:04:52 +0200

    Add empty categories controller

---
+++ src/Modules/Categories/SaveMeter.Modules.Categories.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SaveMeter.Shared.Abstractions.Dispatchers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveMeter.Modules.Categories.Api.Controllers;
+
+[Authorize(Policy)]
+internal class CategoriesController : ControllerBase
+{
+    private const string Policy = "categories";
+    
+    private readonly IDispatcher _dispatcher;
+
+    public CategoriesController(IDispatcher dispatcher)
+    {
+        _dispatcher = dispatcher;
+    }
+}
